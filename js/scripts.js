353694b929709870362eb87ee8b1e250fdbd0b22
agawi88 <agnessn@icloud.com>
2025-01-06 11:27:15 +0100

    changed to button
    used to be crossOutButton

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function newItem() {
-   let li =  $('<li></li>');
-   $('ol').append(li);
+   let li =  $("<li></li>");
+   $("ol").append(li);
    let inputValue = $('#input').val();
    let text = document.createTextNode(inputValue);
    li.append(inputValue);
@@ -22,10 +22,10 @@ function newItem() {
     });
 
     //3(i). Adding the delete button "X":
-    //why isn't the code below working? I found "it" on https://api.jquery.com/add/#add-selector
-    // let crossOutButton = $(li).add(<crossOutButton> 'X' </crossOutButton>);
-    let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode('X'));
+    //why is the code below not working? I found "it" on https://api.jquery.com/add/#add-selector
+    // let crossOutButton = $(li).add(<crossOutButton> "X" </crossOutButton>);
+    let crossOutButton = $("<button></button>");
+    crossOutButton.append(document.createTextNode("X"));
     li.append(crossOutButton);
 
     crossOutButton.on("click", deleteListItem);
@@ -36,7 +36,7 @@ function newItem() {
  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
     
  // 4. Reordering the items: 
-  $('#list').sortable();
+  $("#list").sortable();
   
  // 5. Clear input after adding item
   $("#input").val(""); 
